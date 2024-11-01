6ef8515f633388503a7f57692c69b3b2c37f4adc
Changbeom Choi <me@cbchoi.info>
2024-11-02 00:23:54 +0900

    Missing File Reverted

---
+++ tests/src/TestAtomic.cpp
@@ -0,0 +1,14 @@
+#include "TestAtomic.hpp"
+
+CTestAtomic::CTestAtomic(std::string name)
+	:evsim::CAtomic(name)
+{
+	REGISTER_INPUT_PORT(one);
+	REGISTER_INPUT_PORT(two);
+	REGISTER_INPUT_PORT(three);
+
+	REGISTER_STATE(IDLE1);
+	REGISTER_STATE(IDLE2);
+
+	cur_state = IDLE1;
+}
