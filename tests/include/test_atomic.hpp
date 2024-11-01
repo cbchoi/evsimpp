4b6301118182346100cd267cc8da392e587c5422
Changbeom Choi <me@cbchoi.info>
2024-11-02 00:24:19 +0900

    Another Missing File Reverted

---
+++ tests/include/test_atomic.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "dummy_atomic.hpp"
+#include "gtest/gtest.h"
+
+namespace evsim {
+    // The fixture for testing class Foo.
+    class AtomicTest : public testing::Test {
+    protected:
+        // You can remove any or all of the following functions if their bodies would
+        // be empty.
+
+        AtomicTest()
+            :p1("one"), p2("two"), p3("three")
+        {
+            // You can do set-up work for each test here.
+        }
+
+        ~AtomicTest() override {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
+
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
+
+        void SetUp() override {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+        }
+
+        void TearDown() override {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
+
+        // Class members declared here can be used by all tests in the test suite
+        Port p1; 
+        Port p2; 
+        Port p3; 
+    };
+
+    TEST_F(AtomicTest, test_port)
+    {  
+        CDummyAtomic da = CDummyAtomic("da");
+        {
+            const std::set<Port>::iterator iter = da.in_port().find(Port("two"));
+            ASSERT_NE(iter, da.in_port().end());
+            //std::cout << *iter;
+            EXPECT_EQ(iter->m_name, "two");
+        }
+
+        {
+            const std::set<Port>::iterator iter = da.in_port().find(Port("four"));
+            EXPECT_EQ(iter, da.in_port().end());
+            //if (iter == da.in_port().end())
+            //    std::cout << "EEE";
+        }
+    }
+
+    TEST_F(AtomicTest, test_transitions)
+    {
+        CDummyAtomic da = CDummyAtomic("da");
+        SystemMessage sm = SystemMessage();
+        Port p1 = Port("two");
+        da.external_transition(p1, sm);
+        da.internal_transition();
+    }
+}
+
